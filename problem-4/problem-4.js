3415301219bd3285ead20d8ced0efaf063c845df
Thakur Saad <thakursaad613@gmail.com>
2022-09-09 21:49:40 +0600

    problem 4 solved

---
+++ problem-4/problem-4.js
@@ -0,0 +1,20 @@
+var numberOfSteps = function (num) {
+  let steps = 0;
+  let resultEachTime = num;
+
+  for (let i = resultEachTime; i > 0; i--) {
+    const dividedBy2 = resultEachTime % 2;
+
+    if (resultEachTime == 0) {
+      return steps;
+    }
+
+    if (dividedBy2 == 0) {
+      resultEachTime = resultEachTime / 2;
+      steps++;
+    } else if (dividedBy2 !== 0) {
+      resultEachTime = resultEachTime - 1;
+      steps++;
+    }
+  }
+};
